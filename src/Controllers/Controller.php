960276ea5e4b9d31910fcdf002e4dc71cb738668
Dalibor Marinkovic <dalibor.marinkovic@gmail.com>
2021-10-08 00:33:42 +0200

    obrisan show

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -31,25 +31,7 @@ class Controller{
          }
     }
 
-    public function show($modelId){
-         $result = $this->model->read($modelId);
-         if (! $result) {
-            return $this->notFoundResponse();
-        }
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
-         $row= $result->fetch(PDO::FETCH_ASSOC);
-         if (! $row) {
-            return $this->notFoundResponse();
-        }
-         $in=[
-            "Mentors_Surname"=>$row["Mentors_Surname"],
-            "Mentors_Name"=>$row["Mentors_Name"],
-            "Groups_Title"=>$row["Groups_Title"]
-        ];
-        $response['status_code_header'] = 'HTTP/1.1 201 Created';
-        echo $response['body'] = json_encode($in);
-        return $response;
-    }
+    
 
     public function store()
     {
